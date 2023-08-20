bc8b72692e36d1d188eadf072a8d4463cd506154
RicePandaaa <anthony.ha.pham@gmail.com>
2023-08-20 09:01:27 -0500

    Added 2 commands

--- cogs/general.py
+++ cogs/general.py
@@ -41,7 +41,6 @@ class General(commands.Cog):
                 ret += 1
 
         await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
-
     @commands.hybrid_command()
     async def howdy(self, ctx: Context) -> None:
         """ Basic command for obtaining info about the bot """
@@ -54,10 +53,28 @@ class General(commands.Cog):
         """ Basic command redirecting users to the office hours channel (for now) """
         await ctx.send(f"If there is a link to the office hours times, it will be shown here: {self.office_hours}")
         
-    @commands.hybrid_command(aliases=["chegg", "chatgpt"])
-    async def cheating(self, ctx: Context):
+    @commands.hybrid_command()
+    async def cheating(self, ctx: Context) -> None:
         """ Basic command in regards of sus study material sources """
-        await ctx.send("Please do not use outside resources such as Chegg and ChatGPT to do your assignments!")
+        await ctx.send("Please do not use outside resources such as Chegg and ChatGPT to do your assignments!" \
+                       " Also, if you are helping others, do be aware to not share your solutions or to share or say anything" \
+                        " that may break the Aggie Honor Code and any rules set by your professor.")
+
+    @commands.hybrid_command()
+    async def explain(self, ctx: Context) -> None:
+        """ Basic command in case the question is asked is very vague """
+        formatting_link = "https://support.discord.com/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline-"
+        await ctx.send(f"{ctx.author.display_name} wants to help you, but it seems that your question is rather unclear." \
+                       f" For coding questions, please consult <{formatting_link}> to properly show code snippets, but DO NOT SHARE CODE USED IN YOUR SUBMITTED ASSIGNMENTS." \
+                        f" Also please provide full error messages for best results (you may explain the error but again, be sure your explanation is clear.)" \
+                         f" For any other questions, please provide full context of your problem, including any related images that can help.")
+        
+    @commands.hybrid_command()
+    async def code(self, ctx: Context) -> None:
+        """ Basic command warning users against posting code for their HW or other assignments """
+        await ctx.send("DO NOT SHARE ANY PART OF YOUR CODE THAT IS USED IN YOUR SUBMITTED ASSIGNMENTS." \
+                       "You can only share code that was created for purposes unrelated to your assignments." \
+                        " Posting your code used in assignments (such as labs and HW) can and will lead to severe consequences.")
         
     @commands.hybrid_command()
     @commands.has_any_role('TAO Officer')
@@ -81,7 +98,7 @@ class General(commands.Cog):
         vc_cog.add_queue_channel(channel_id, role_id)
 
         # Confirm to the user
-        await ctx.send(f'You have set the "{voice_channel.name}" voice channel as a queue channel. Those with the "{subject_role}" role will be able to pull students from that channel into their office hours channel.')
+        await ctx.send(f'You have set the "{voice_channel.name}" voice channel as a queue channel. PTs with the "{subject_role}" role will be able to pull students from that channel into their office hours channel.')
 
     @commands.hybrid_command()
     @commands.has_any_role('TAO Officer')
