9c1ee49d3524ec531f079327750907780b6f2e0d
Ricepandaaaa <anthony.ha.pham@gmail.com>
2023-12-15 14:15:53 -0600

    Make TAO resources embed command

--- cogs/general.py
+++ cogs/general.py
@@ -9,6 +9,10 @@ class General(commands.Cog):
         self.bot = bot
         self.office_hours = "\"DOES NOT EXIST YET\""
 
+        self.review_216 = "None"
+        self.review_217 = "None"
+        self.review_102 = "None"
+
     @commands.hybrid_command()
     @commands.guild_only()
     @commands.has_any_role('TAO Officer')
@@ -217,6 +221,32 @@ class General(commands.Cog):
             "Info for New PTs (Google Doc)": "https://docs.google.com/document/d/1JvCgARe7d32JmA3MvYfoq2PwR3YdfZpuK48feHKUvtk/edit#heading=h.gwwqgp1crfcc",
             "How to Log Hours (Discord Message Chain)": "https://discord.com/channels/1022962971607060540/1143205554362269788/1143936580243968090",
             "216/217 Lab - Helpful Info (Google Slides)": "https://docs.google.com/presentation/d/10ZxiElBy0SsB5s-FwOXpJD-jiORtwtdnGqKG4CxraZE/edit#slide=id.p"
+        }
+
+        # Make the embed
+        embed = discord.Embed(color=discord.Color.dark_red())
+        embed.set_author(name="Helpful Resources for PTs!")
+
+        # Add the fields
+        for field_name in fields:
+            embed.add_field(name=field_name, value=fields[field_name], inline=False)
+
+        # Send the embed
+        await ctx.send(embed=embed)
+
+    @commands.hybrid_command()
+    async def taoresources(self, ctx: Context):
+        """ Sends an embed with links to TAO materials"""
+
+        # Field contents
+        fields = {
+            "Reviews and Resources from previous and current semesters can be found in the TAO Drive!": "",
+            "TAO Drive": "https://tx.ag/taoreviewdrive",
+            "216/217 Lab Helpful Info": "https://docs.google.com/presentation/d/10ZxiElBy0SsB5s-FwOXpJD-jiORtwtdnGqKG4CxraZE/edit#slide=id.p",
+            "102 Helpful Info": "In progress!",
+            "216 Review (Current Semester)": self.review_216,
+            "217 Review (Current Semester)": self.review_217,
+            "102 Review (Current Semester)": self.review_102
         }
 
         # Make the embed
