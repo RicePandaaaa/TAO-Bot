4611059fcb8f49b4bc75163a95ee74b359579b61
Ricepandaaaa <anthony.ha.pham@gmail.com>
2023-12-15 08:03:41 -0600

    Set template for prof list edit command

---
+++ cogs/professor_editor.py
@@ -0,0 +1,20 @@
+import discord, csv, os
+from discord.ext import commands
+from discord.ext.commands import Context
+
+
+class ProfEditor(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.hybrid_command()
+    @commands.has_any_role("TAO Officer")
+    async def set_professors(self, ctx: Context, 
+                             class_name: str = commands.Parameter(description="Name of the class to change (case-sensitive)"),
+                             prof_names: str = commands.Parameter(description="List of all professor names, separated with only commas")):
+        pass
+
+
+async def setup(bot):
+    await bot.add_cog(ProfEditor(bot))
